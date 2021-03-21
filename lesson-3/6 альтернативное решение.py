626f51728815987cce742c4749c556e1f203bf72
Semen Kulikov <61802690+Semyonnnnn@users.noreply.github.com>
2021-03-21 20:58:38 +0300

    Delete lesson-3 directory

--- lesson-3/6 альтернативное решение.py
+++
@@ -1,7 +0,0 @@
-# Для подсчёта количества букв в тексте используем функцию Counter
-
-from collections import Counter
-text = '[Введите текст]' 
-print(' {:*^30} '.format(text))
-s = str(input())
-print(Counter(s))
