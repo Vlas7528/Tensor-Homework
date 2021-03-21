ac559fcb121a89514c78495533672de032f51ee1
Semen Kulikov <61802690+Semyonnnnn@users.noreply.github.com>
2021-03-21 20:59:03 +0300

    Delete lesson-5 directory

--- lesson-5/5 - подсчёт частоты символов.py
+++
@@ -1,4 +0,0 @@
-from collections import Counter
-count = lambda text: Counter(text)
-text = input('Введите текст: \n')
-print(f'Частота символов в тексте:\n{count(text)}')
--- lesson-5/7 - вычисление факториала рекурсией lambda.py
+++
@@ -1,9 +0,0 @@
-from functools import reduce
-while True:
-    n=input()
-    try:
-        n=int(n)
-        break
-    except:
-        print('Неверный формат числа')
-print (reduce(lambda x,y:x*y,range(1,n+1)))
